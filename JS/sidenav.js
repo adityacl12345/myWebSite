7d5406c8fa57e4ba5ce8cd5f6a8c5bf2fd90acaa
Aditya Ghosh <adghosh@atlassian.com>
2018-06-14 18:56:08 +0200

    Minor changes in side navigation bar

--- JS/sidenav.js
+++ JS/sidenav.js
@@ -1,6 +1,7 @@
 window.onload = function(){
     
-        document.getElementById("sidebar").addEventListener('click',openCloseBar);
+    document.getElementById("sidebar").addEventListener('click',openCloseBar);
+    document.body.addEventListener('click', closeBar);
 }
 function openCloseBar(){
     var sidenav = document.getElementById("sidenav");
@@ -10,4 +11,13 @@ function openCloseBar(){
     else{
         sidenav.style.width = "0px";
     }
+}
+function closeBar(event) {
+    var sidenav = document.getElementById("sidenav");
+    var noClick = "#sidebar";
+    if(!event.target.matches(noClick)) {
+        if(sidenav.style.width == "250px"){
+            sidenav.style.width = "0px";
+        }
+    }  
 }
